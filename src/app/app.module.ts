5c4ed2701597676d53ea79b2172769cd69edf765
Tytus Planck <tytusplanck@gmail.com>
2017-07-28 16:27:02 -0400

    Added home component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+RouterModule.forRoot([
+{
+path: 'home',
+component: HomeComponent
+}
+])
 
 @NgModule({
   declarations: [
